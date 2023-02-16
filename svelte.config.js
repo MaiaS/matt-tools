7ee9a39f42f3af9c55783bd339e21677167871cd
Maia Sason (work) <maia@lumber.dev>
2023-02-15 20:56:23 -0500

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,19 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		prerender: {
+			default: true
+		},
+		trailingSlash: 'always',
+		paths: {
+			base: '/matt-tools'
+		}
 	}
 };
 
